a6b133843c4fbd27476ff5a8cac8be36221a1635
weitao23333 <weitao23333@outlook.com>
2023-07-18 04:16:26 +0800

    start

---
+++ zerogravity/Plugin.cs
@@ -0,0 +1,20 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+
+namespace zerogravity
+{
+    [BepInPlugin(("weitao.mm." + PluginInfo.PLUGIN_GUID), PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private ConfigEntry<float> gravityconfigX;
+        private ConfigEntry<float> gravityconfigY;
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+            gravityconfigX = Config.Bind("config","gravityconfigX",(float)0);gravityconfigY = Config.Bind("config","gravityconfigY",(float)-0.5);
+            UnityEngine.Physics2D.gravity = new UnityEngine.Vector2(gravityconfigX.Value,gravityconfigY.Value);
+
+        }
+    }
+}
---
+++ zerogravity/obj/Debug/netstandard2.0/PluginInfo.cs
@@ -0,0 +1,9 @@
+namespace zerogravity
+{
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "zerogravity";
+        public const string PLUGIN_NAME = "zerogravity";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
